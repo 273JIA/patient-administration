3991a230aa635e8c8b18ad3e4521e63c00f37089
Jia273 <114813337+01226@users.noreply.github.com>
2024-07-13 16:57:27 +0100

    first commit

---
+++ App/models.py
@@ -0,0 +1,51 @@
+from email import message
+from django.db import models
+
+# PATIENT
+class Patient(models.Model):
+
+    GENDER = (
+        ('M', 'M'),
+        ('F', 'F'),
+    )
+
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=40)
+    phone = models.CharField(max_length=20)
+    email = models.CharField(max_length=40)
+    age = models.CharField(max_length=3)
+    gender = models.CharField(max_length=1, null=True, choices=GENDER)
+    note = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+# SUPPORT
+OPTION = (
+    ('It heppened to me', 'It heppened to me'),
+    ('It was already like that', 'It was already like that'),
+)
+
+REASON = (
+    ('Delete patient', 'Delete patient'),
+    ('System problems', 'System problems'),
+    ('Others', 'Others'),
+)
+
+SITUATION = (
+    ('Done', 'Done'),
+    ('Pending', 'Pending'),
+)
+
+class Call(models.Model):
+    terms = models.BooleanField("You got this responsability")
+    user = models.CharField(max_length=100)
+    message = models.TextField()
+    option = models.CharField(max_length=100, choices=OPTION)
+    reason = models.CharField(max_length=100, choices=REASON)
+    created_at = models.DateTimeField(auto_now_add=True)
+    Situation = models.CharField(max_length=100, null=True, choices=SITUATION, default='Pending')
+
+    def __str__(self):
+        return self.user
---
+++ patients/urls.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from django.urls import path, include
+from App import views
+
+urlpatterns = [
+    # Native path to access the django admin
+    path('admin/', admin.site.urls),
+    # Path to access the frontend page
+    path('', views.frontend, name="frontend"),
+    # Path to login / logout
+    path('login/', include('django.contrib.auth.urls')),
+
+    # --------------------- BACKEND SECTION ---------------------|
+    # Path to Access the backend page
+    path('backend/', views.backend, name="backend"),
+    # Path to Add patient
+    path('add_patient', views.add_patient, name="add_patient"),
+    # Path to Access the patient individually
+    path('patient/<str:patient_id>', views.patient, name="patient"),
+    # Path to Edit patient
+    path('edit_patient', views.edit_patient, name="edit_patient"),
+    # Path to Delete the patient
+    path('delete_patient/<str:patient_id>', views.delete_patient, name="delete_patient"),
+
+    # Support
+    path('support', views.support, name="support"),
+]
